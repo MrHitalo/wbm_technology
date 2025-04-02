550bdce7adfa077471c9bf6632d8e584d519a4cc
MrHitalo <hitalofariaalmeida1602@gmail.com>
2025-04-02 15:01:29 -0300

    .

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,3 @@
 import { Routes } from '@angular/router';
-import { PainelComponent } from './painel/painel.component';
-import { TodosDispositivosComponent } from './painel/todos-dispositivos/todos-dispositivos.component';
 
-export const routes: Routes = []
+export const routes: Routes = [];
