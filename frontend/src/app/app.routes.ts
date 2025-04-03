963d63baedc6d8e52d8c268519ca0fbe663bb9d5
pedrolucas444 <plsousasilva1000@gmail.com>
2025-04-03 11:52:21 -0300

    teste

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { PainelComponent } from './painel/painel.component';
 import { Component } from '@angular/core';
-import { TodosDispositivosComponent } from './painel/todos-dispositivos/todos-dispositivos.component';
 
 export const routes: Routes = [
     {
         path:'',
         component: PainelComponent
-    },
-    {
-        path: 'todos',
-        component: TodosDispositivosComponent
     }
 ];
