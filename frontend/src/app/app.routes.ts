323620c1fc7050fe5db7ce4a84f271052b0f6482
MrHitalo <hitalofariaalmeida1602@gmail.com>
2025-04-03 09:37:30 -0300

    .

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PainelComponent } from './painel/painel.component';
+import { Component } from '@angular/core';
+import { TodosDispositivosComponent } from './painel/todos-dispositivos/todos-dispositivos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: PainelComponent
+    },
+    {
+        path: 'todos',
+        component: TodosDispositivosComponent
+    }
+];
--- frontend/src/app/painel/painel.component.ts
+++ frontend/src/app/painel/painel.component.ts
@@ -3,12 +3,54 @@ import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from '../components/header/header.component';
 import { CardEquipamentoComponent } from '../components/card-equipamento/card-equipamento.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
+interface Dispositivo {
+  key: string;
+  value: { [key: string]: number };
+}
 
 @Component({
   selector: 'app-painel',
-  imports: [MatButtonModule, HeaderComponent, CardEquipamentoComponent, MatGridListModule], // Importando corretamente o módulo do botão
+  imports: [MatButtonModule, HeaderComponent, CardEquipamentoComponent, MatGridListModule, CommonModule], // Importando corretamente o módulo do botão
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css'] // Nome correto da propriedade
 })
-export class PainelComponent { }
+export class PainelComponent { 
+
+  dadosModbus: { [key: string]: { [key: string]: number } } = {};
+  dispositivosFormatados: Dispositivo[] = [];
+  carregando = true;
+  erroCarregamento = '';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.carregarDados();
+  }
+
+  private carregarDados() {
+    this.http.get<{ data: any }>('http://localhost:3000/api/modbus/data').subscribe({
+      next: (res) => {
+        this.dadosModbus = res.data;
+        this.dispositivosFormatados = Object.entries(res.data).map(([key, value]) => ({
+          key,
+          value: value as { [key: string]: number }
+        }));
+        this.carregando = false;
+      },
+      error: (err) => {
+        this.erroCarregamento = 'Falha ao conectar com o servidor Modbus';
+        this.carregando = false;
+        console.error('Erro:', err);
+      }
+    });
+  }
+
+  formatarValor(valor: any): string {
+    const num = Number(valor);
+    return isNaN(num) ? 'N/A' : num.toString();
+  }
+  
+}
