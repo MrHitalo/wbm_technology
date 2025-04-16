a840b936e190f1ca58bd648049a0d860e6fbae19
pedrolucas444 <plsousasilva1000@gmail.com>
2025-04-16 17:52:24 -0300

    atualizações finais css

--- frontend/src/components/TabelaDeErros.tsx
+++ frontend/src/components/TabelaDeErros.tsx
@@ -19,20 +19,21 @@ export default function TabelaDeErros({ tituloTabela = "Tabela de Erros", erros 
         <h4 className="font-bold text-xl mb-10 text-center text-gray-800">{tituloTabela}</h4>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-4 mt-4">
-          {erros.map((erro, index) => (
-            <div
-              key={index}
-              className={`flex justify-between items-center ${erro.full ? "md:col-span-2" : ""
-                }`}
-            >
-              <div className="border-b-gray-400 border-b-2 w-full ">
-                <h6 className="text-lg font-semibold text-gray-900">{erro.titulo}:</h6>
-              </div>
-              {/* <Eye className="text-gray-600 w-5 h-5" /> */}
-            </div>
-          ))}
-
-        </div>
+  {erros.map((erro, index) => (
+    <div
+      key={index}
+      className={`flex items-center ${erro.full ? "md:col-span-2" : ""}`}
+    >
+      <div className="border-b-2 border-b-gray-400 w-full text-left">
+        <h6 className="text-lg font-semibold text-gray-900">
+          {erro.titulo}:
+        </h6>
+      </div>
+      {/* Se for usar o ícone, deixa aqui ao lado */}
+      {/* <Eye className="text-gray-600 w-5 h-5 ml-2" /> */}
+    </div>
+  ))}
+</div>
       </div>
     </div>
   );
